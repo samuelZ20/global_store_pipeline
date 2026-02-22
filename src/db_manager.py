08ae95228c488d8e44bf3ed394810c047db73e85
samuelZ20 <frizzonecardoso@gmail.com>
2026-02-22 13:11:59 -0300

    fix: criar a tabela

--- src/db_manager.py
+++ src/db_manager.py
@@ -2,6 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
 from dotenv import load_dotenv
+from sqlalchemy import text
 
 # Carrega as credenciais do .env
 load_dotenv()
@@ -26,5 +27,23 @@ def get_engine():
         print(f"❌ Erro ao conectar ao banco de dados: {e}")
         return None
 
+
+def create_tables(engine):
+    """Cria a tabela explicitamente se ela não existir."""
+    query = """
+    CREATE TABLE IF NOT EXISTS silver_products (
+        id INT PRIMARY KEY,
+        title TEXT,
+        price FLOAT,
+        category TEXT,
+        image TEXT,
+        rating_rate FLOAT,
+        rating_count INT,
+        extraction_timestamp TIMESTAMP
+    );
+    """
+    with engine.connect() as conn:
+        conn.execute(text(query))
+        conn.commit()
 if __name__ == "__main__":
     get_engine()
